5b1da32e24255f396d896d597b6080cb20611194
abdullahsiam3000 <abdullahsiam004@gmail.com>
2024-03-05 00:16:33 +0600

    active menu

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,12 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
+import { PiBugBeetleLight } from "react-icons/pi";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+  console.log(currentPath);
   const links = [
     {
       label: "Dashboard",
@@ -14,12 +19,16 @@ const NavBar = () => {
   ];
   return (
     <nav className="flex border-b px-4 h-14 items-center gap-6">
-      <Link href={"/"}>Issue Tracker</Link>
+      <Link href={"/"}>
+        <PiBugBeetleLight size={"2rem"} className="text-zinc-500" />
+      </Link>
       <ul className="flex gap-4">
         {links.map((link) => (
           <Link
             key={link.href}
-            className="text-zinc-400 hover:text-zinc-800 transition-colors"
+            className={`${
+              currentPath === link.href ? "text-zinc-800" : "text-zinc-400"
+            } hover:text-zinc-800 transition-colors `}
             href={link.href}
           >
             {link.label}
