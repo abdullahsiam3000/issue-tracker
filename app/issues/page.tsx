b33e493e7d0bb7e7e922a1e466592003166dd063
abdullahsiam3000 <abdullahsiam004@gmail.com>
2024-03-05 18:20:15 +0600

    show the issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,15 +1,48 @@
-import { Button } from "@radix-ui/themes";
-import Link from "next/link";
-import React from "react";
-
-const IssuePage = () => {
+import { Button, Table } from '@radix-ui/themes'
+import Link from 'next/link'
+import React from 'react'
+import prisma from '@/prisma/client'
+const IssuePage = async () => {
+  const issues = await prisma.issue.findMany()
+  console.log(issues)
   return (
     <div>
-      <Button>
-        <Link href={"/issues/new"}>Create Issue</Link>
-      </Button>
-    </div>
-  );
-};
+      <div className='mb-5'>
+        <Button>
+          <Link href={'/issues/new'}>Create Issue</Link>
+        </Button>
+      </div>
+      <Table.Root variant='surface'>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className='hidden md:table-cell'>
+              Description
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className='hidden md:table-cell'>
+              Created
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
 
-export default IssuePage;
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                {issue.title} <div className='block md:hidden'>{issue.status}</div>
+              </Table.Cell>
+              <Table.Cell className='hidden md:table-cell'>{issue.description}</Table.Cell>
+              <Table.Cell className='hidden md:table-cell'>{issue.status}</Table.Cell>
+              <Table.Cell className='hidden md:table-cell'>
+                {issue.createdAt.toLocaleDateString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </div>
+  )
+}
+
+export default IssuePage
