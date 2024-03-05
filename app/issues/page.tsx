2282b668c3107feed5ad2036a98c5783b66202b1
abdullahsiam3000 <abdullahsiam004@gmail.com>
2024-03-05 20:06:27 +0600

    show issue detail

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -4,6 +4,7 @@ import prisma from '@/prisma/client'
 import IssueStatusBadge from '../components/IssueStatusBadge'
 import delay from 'delay'
 import IssueActions from './IssueActions'
+import Link from 'next/link'
 const IssuePage = async () => {
   const issues = await prisma.issue.findMany()
   await delay(2000)
@@ -28,7 +29,7 @@ const IssuePage = async () => {
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                {issue.title}
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
                 <div className='mt-2 block md:hidden'>
                   <IssueStatusBadge status={issue.status} />
                 </div>
