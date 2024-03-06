98a696a1dda35f277b78f444624234c7a877056d
abdullahsiam3000 <abdullahsiam004@gmail.com>
2024-03-06 13:45:04 +0600

    add additional loading skeleton

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import prisma from '@/prisma/client'
 import { notFound } from 'next/navigation'
-import { Card, Flex, Heading, Text } from '@radix-ui/themes'
+import { Box, Card, Flex, Heading, Text } from '@radix-ui/themes'
 import { capitalizeFirstLetter } from '@/app/utils/capitalizeFirstLetter'
 import IssueStatusBadge from '@/app/components/IssueStatusBadge'
 import Markdown from 'react-markdown'
+
 interface Props {
   params: { id: string }
 }
@@ -12,8 +13,9 @@ interface Props {
 const IssueDetailPage = async ({ params }: Props) => {
   const issue = await prisma.issue.findUnique({ where: { id: parseInt(params.id) } })
   if (!issue) notFound()
+
   return (
-    <div>
+    <Box>
       <Heading>{capitalizeFirstLetter(issue?.title)}</Heading>
       <Flex gap={'2'} my={'3'}>
         <Text>
@@ -24,7 +26,7 @@ const IssueDetailPage = async ({ params }: Props) => {
       <Card className='prose' mt={'6'}>
         <Markdown>{issue?.description}</Markdown>
       </Card>
-    </div>
+    </Box>
   )
 }
 
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -3,11 +3,11 @@ import Link from '../components/Link'
 import React from 'react'
 import prisma from '@/prisma/client'
 import IssueStatusBadge from '../components/IssueStatusBadge'
-import delay from 'delay'
+
 import IssueActions from './IssueActions'
 const IssuePage = async () => {
   const issues = await prisma.issue.findMany()
-  await delay(2000)
+
   return (
     <div>
       <IssueActions />
