c9a7c4264dc08d782bf81209fee7d027ae8373c9
abdullahsiam3000 <abdullahsiam004@gmail.com>
2024-03-11 22:54:10 +0600

    secure api route

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -4,12 +4,16 @@ import { notFound } from 'next/navigation'
 import EditIssueButton from './EditIssueButton'
 import IssueDetails from './IssueDetails'
 import DeleteIssueButton from './DeleteIssueButton'
+import { getServerSession } from 'next-auth'
+import authOptions from '@/app/auth/authOptions'
 
 interface Props {
   params: { id: string }
 }
 
 const IssueDetailPage = async ({ params }: Props) => {
+  const session = await getServerSession(authOptions)
+
   const issue = await prisma.issue.findUnique({ where: { id: parseInt(params.id) } })
   if (!issue) notFound()
 
@@ -18,12 +22,14 @@ const IssueDetailPage = async ({ params }: Props) => {
       <Box className='md:col-span-4'>
         <IssueDetails issue={issue} />
       </Box>
-      <Box>
-        <Flex direction={'column'} gap={'4'}>
-          <EditIssueButton issueId={parseInt(params.id)} />
-          <DeleteIssueButton issueId={parseInt(params.id)} />
-        </Flex>
-      </Box>
+      {session && (
+        <Box>
+          <Flex direction={'column'} gap={'4'}>
+            <EditIssueButton issueId={parseInt(params.id)} />
+            <DeleteIssueButton issueId={parseInt(params.id)} />
+          </Flex>
+        </Box>
+      )}
     </Grid>
   )
 }
