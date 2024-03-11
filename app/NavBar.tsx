a2124a02ec0fd1aae9500581971a49a9b3e89d1d
abdullahsiam3000 <abdullahsiam004@gmail.com>
2024-03-11 13:37:53 +0600

    navbar refactor

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -9,8 +9,55 @@ import { usePathname } from 'next/navigation'
 import { PiBugBeetleLight } from 'react-icons/pi'
 import { Skeleton } from '@/app/components'
 const NavBar = () => {
-  const currentPath = usePathname()
+  return (
+    <nav className='mb-2  border-b px-4 py-3'>
+      <Container>
+        <Flex align={'center'} justify={'between'}>
+          <Flex align={'center'} gap={'3'}>
+            <Link href={'/'}>
+              <PiBugBeetleLight size={'2rem'} className='text-zinc-500' />
+            </Link>
+            <NavLinks />
+          </Flex>
+          <AuthStatus />
+        </Flex>
+      </Container>
+    </nav>
+  )
+}
+
+const AuthStatus = () => {
   const { status, data: session } = useSession()
+  if (status === 'loading') return <Skeleton width='3rem' />
+  if (status === 'unauthenticated') return <Link href={'/api/auth/signin'}>Login</Link>
+
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            fallback='?'
+            src={session!.user!.image!}
+            size={'2'}
+            radius='full'
+            className='cursor-pointer'
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>
+            <Text size={'3'}>{session!.user?.email}</Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Item>
+            <Link href={'/api/auth/signout'}>Logout</Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
+  )
+}
+
+const NavLinks = () => {
+  const currentPath = usePathname()
 
   const links = [
     {
@@ -24,57 +71,21 @@ const NavBar = () => {
   ]
 
   return (
-    <nav className='mb-2  border-b px-4 py-3'>
-      <Container>
-        <Flex align={'center'} justify={'between'}>
-          <Flex align={'center'} gap={'3'}>
-            <Link href={'/'}>
-              <PiBugBeetleLight size={'2rem'} className='text-zinc-500' />
-            </Link>
-            <ul className='flex gap-4'>
-              {links.map((link) => (
-                <li key={link.href}>
-                  <Link
-                    className={classNames('transition-colors hover:text-zinc-800', {
-                      'text-zinc-900': currentPath.split('/')[1] === link.href.split('/')[1],
-                      'text-zinc-400': currentPath !== link.href,
-                    })}
-                    href={link.href}
-                  >
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </Flex>
-          <Box>
-            {status === 'authenticated' && (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    fallback='?'
-                    src={session.user!.image!}
-                    size={'2'}
-                    radius='full'
-                    className='cursor-pointer'
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                  <DropdownMenu.Label>
-                    <Text size={'3'}>{session.user?.email}</Text>
-                  </DropdownMenu.Label>
-                  <DropdownMenu.Item>
-                    <Link href={'/api/auth/signout'}>Logout</Link>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            )}
-            {status === 'loading' && <Skeleton width='3rem' />}
-            {status === 'unauthenticated' && <Link href={'/api/auth/signin'}>Login</Link>}
-          </Box>
-        </Flex>
-      </Container>
-    </nav>
+    <ul className='flex gap-4'>
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            className={classNames('transition-colors hover:text-zinc-800', {
+              'text-zinc-900': currentPath.split('/')[1] === link.href.split('/')[1],
+              'text-zinc-400': currentPath !== link.href,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
   )
 }
 
