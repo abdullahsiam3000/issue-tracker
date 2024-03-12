681c66f5b3937f723c89742c0d9fdfe96b6029e0
abdullahsiam3000 <abdullahsiam004@gmail.com>
2024-03-13 01:41:06 +0600

    creating get user route and fetching users in assignee compoent

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import prisma from '@/prisma/client'
 export async function GET(request: NextRequest) {
   try {
-    const users = await prisma.user.findMany()
+    const users = await prisma.user.findMany({ orderBy: { name: 'asc' } })
     if (users) {
       return NextResponse.json(users)
     }
