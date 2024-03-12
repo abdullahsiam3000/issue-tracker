c91d67d36544e8068034bc4d48df1bef4cc45d4e
abdullahsiam3000 <abdullahsiam004@gmail.com>
2024-03-13 01:31:15 +0600

    populate assignee select compoent

---
+++ app/api/users/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from 'next/server'
+import prisma from '@/prisma/client'
+export async function GET(request: NextRequest) {
+  try {
+    const users = await prisma.user.findMany()
+    if (users) {
+      return NextResponse.json(users)
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
--- app/issues/_components/AssigneeSelect.tsx
+++ app/issues/_components/AssigneeSelect.tsx
@@ -1,16 +1,36 @@
 'use client'
+import { User } from '@prisma/client'
 import { Select } from '@radix-ui/themes'
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const AssigneeSelect = () => {
+  const [users, setUsers] = useState<User[]>([])
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const { data } = await axios.get<User[]>('http://localhost:3000/api/users')
+        setUsers(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    fetchUsers()
+  }, [])
+
+  console.log(users, 'users')
   return (
     <Select.Root onValueChange={(value) => console.log(value)}>
       <Select.Trigger placeholder='Assignee' />
       <Select.Content>
         <Select.Group>
-          <Select.Item value='1'>Siam</Select.Item>
-          <Select.Item value='2'>Itachi</Select.Item>
-          <Select.Item value='3'>Minato</Select.Item>
+          {users.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
