b8ae3d546e5ffec8a6fcc8873f76ef89bb06b374
abdullahsiam3000 <abdullahsiam004@gmail.com>
2024-03-18 03:46:33 +0600

    minor

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,8 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server'
 import prisma from '@/prisma/client'
+
 export async function GET(request: NextRequest) {
   try {
     const users = await prisma.user.findMany({ orderBy: { name: 'asc' } })
+    console.log(users)
     if (users) {
       return NextResponse.json(users)
     }
